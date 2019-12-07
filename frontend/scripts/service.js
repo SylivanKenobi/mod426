91e65933ed0c99d15f8c3ed21715293264dc0349
Your Name <you@example.com>
2019-12-07 09:23:11 +0100

    gets the lesson from the backend to the frontend

--- frontend/scripts/service.js
+++ frontend/scripts/service.js
@@ -1,7 +1,9 @@
 // gets the object from the backend
 function httpGet(discipline)
 {
-    $.getJSON("http://localhost:8080/api/discipline/" + discipline, function(discipline) {
-        console.log(discipline);
+   return $.ajax({
+        url: `http://localhost:8080/api/discipline/${discipline}`,
+        type: 'GET',
+        datatype: 'json',
     });
 }
